a8dd11c18131fd8398c66d8ecaf6263524b0fec8
ZORO <rs3274357@gmail.com>
2023-12-22 11:07:04 +0530

    Implement the logic (choose button, Compress button)

--- ZipperMagic.py
+++ ZipperMagic.py
@@ -1,20 +1,31 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
 lable1 = sg.Text('Select files to compress:')
 input1 = sg.Input()
-choose_button1 = sg.FileBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 lable2 = sg.Text('Select destination compress:')
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+output_lable = sg.Text(key="output_lable", text_color="green")
 
 window = sg.Window("Zipper Magic",
                    layout=[[lable1, input1, choose_button1],
                            [lable2, input2, choose_button2],
-                           [compress_button]])
-window.read()
+                           [compress_button],[output_lable]])
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+
+    window["output_lable"].update(value="Compression Completed!")
+
 window.close()
 
 
