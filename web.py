cb5c8b644325c6a0a4b94ce65cad8f0cb98f753b
ZORO <rs3274357@gmail.com>
2023-12-23 10:26:13 +0530

    Implement "Add new todo items" feature

--- web.py
+++ web.py
@@ -2,10 +2,18 @@ import streamlit as st
 import funtions
 
 todos = funtions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+    todos.append(todo + '\n')
+    funtions.set_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to incress your productivity.")
 for todo in todos:
     st.checkbox(todo)
-st.text_input(label="", placeholder="Add new Todo here")
+st.text_input(label="", placeholder="Add new Todo here",
+              on_change=add_todo, key='new_todo')
 
